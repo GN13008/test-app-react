f7a4c31e9506c04b3f2fb72805fdcc1d72cbaf40
Guillaume Negro <negro.guillaume@gmail.com>
2021-10-19 14:31:43 -0400

    composants et props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Guillaume',
+  lastName: 'Negro'
+};
+
+function UserName(props) {
+  return (
+    <h1>Bonjour, {props.fullName} !</h1>
+  )
+}
+
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Bonjour, monde !</h1>
+//       <h2>Il est {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// }
+
+// function tick() {
+//   ReactDOM.render(
+//     <Clock date={new Date()} />,
+//     document.getElementById('root')
+//   );
+// }
+
+// setInterval(tick, 1000);
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <UserName fullName={formatName(user)} />
         <a
           className="App-link"
           href="https://reactjs.org"
